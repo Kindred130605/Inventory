9b3d5971ee647ddc5a72bae5bc3a4880656c7948
panya22 <patricknuelpacis@gmail.com>
2024-05-13 01:55:26 +0800

    pat commit (1)

---
+++ src/service/axiosApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:8000/api/"; // Your API base URL
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    // Add any common headers here
+  },
+});
+
+export default api;
